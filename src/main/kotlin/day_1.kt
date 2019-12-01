a9fcdfa88f03d2a4a6cc3ee620feac0d221f28f8
Pedro Rodriguez Perez <pedrodper@gmail.com>
2019-12-01 19:25:19 +0100

    Day 1: Finished!

---
+++ src/main/kotlin/day_1.kt
@@ -0,0 +1,31 @@
+import java.io.File
+import kotlin.math.floor
+
+const val rootPath = "/home/pedro/Projects/KOTLIN/AoC_19/"
+const val mainPath = rootPath + "src/main/"
+
+fun readInput(path: String): List<Int> {
+    val file = File(path)
+    return file.readText().lines().map{ it.toInt() }
+}
+
+fun calculateSingleModuleFuel(mass: Int): Int = floor(mass.toDouble() / 3.0).toInt() - 2
+
+fun calculateAccumulateSingleModuleFuel(mass: Int): Int {
+    var nextFuelStep = calculateSingleModuleFuel(mass)
+    var completeFuel = nextFuelStep
+    while (nextFuelStep > 6) {
+        val currentFuelStep = calculateSingleModuleFuel(nextFuelStep)
+        completeFuel += currentFuelStep
+        nextFuelStep = currentFuelStep
+    }
+    return completeFuel
+}
+
+fun main(){
+    val input = readInput(mainPath + "resources/day_1.txt")
+    val result1 = input.map{ calculateSingleModuleFuel(it) }.sum()
+    val result2 = input.map{ calculateAccumulateSingleModuleFuel(it) }.sum()
+    print("Task 1: $result1\n")
+    print("Task 2: $result2\n")
+}
---
+++ src/test/kotlin/test_day_1.kt
@@ -0,0 +1,28 @@
+import kotlin.test.assertEquals
+import org.junit.Test as test
+
+class TestSource() {
+
+    val rootPath = "/home/pedro/Projects/KOTLIN/AoC_19/"
+    val testPath = rootPath + "src/test/"
+
+    @test fun test_readInput() {
+        val input = readInput(testPath + "resources/test_day_1_puzzle_1.txt")
+        assertEquals(input, listOf(12, 14, 1969, 100756))
+    }
+
+    @test fun test_calculateSingleModuleFuel() {
+        val input = readInput(testPath + "resources/test_day_1_puzzle_1.txt")
+        assertEquals(input.map{ calculateSingleModuleFuel(it) }, listOf(2, 2, 654, 33583))
+    }
+
+    @test fun test_fuelSum() {
+        val input = readInput(testPath + "resources/test_day_1_puzzle_1.txt")
+        assertEquals(input.map{ calculateSingleModuleFuel(it) }.sum(), 34241)
+    }
+
+    @test fun test_calculateAccumulateSingleModuleFuel() {
+        val input = readInput(testPath + "resources/test_day_1_puzzle_2.txt")
+        assertEquals(input.map{ calculateAccumulateSingleModuleFuel(it) }, listOf(2, 966, 50346))
+    }
+}
